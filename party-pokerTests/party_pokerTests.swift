f3e770c8240de2c4ecb3c2eb03652562c818d75e
Bruce Wang <88688323+BruceWangyq@users.noreply.github.com>
2025-08-14 00:28:05 -0400

    Initial Commit

---
+++ party-pokerTests/party_pokerTests.swift
@@ -0,0 +1,17 @@
+//
+//  party_pokerTests.swift
+//  party-pokerTests
+//
+//  Created by wang yuqiu on 2025-08-14.
+//
+
+import Testing
+@testable import party_poker
+
+struct party_pokerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
